27951e34033eb54c0b5526fd4322d29db2cbfa55
vanthaita <thaitv225@gmail.com>
2025-02-20 21:11:49 +0700

    feat(layout.tsx): Add Suspense and RippleLoader for loading state
     feat(loading.tsx): Create a RippleLoader component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,21 +1,25 @@
 import { SidebarProvider } from '@/components/ui/sidebar'
-import React from 'react'
+import React, { Suspense } from 'react'
 import AppSidebar from './appSidebar'
 import { Toaster } from "@/components/ui/toaster"
+import RippleLoader from '../loading'
+
 type Props = {
     children: React.ReactNode
 }
 
-const SidebarLayout = ({children}: Props) => {
+const SidebarLayout = ({ children }: Props) => {
   return (
     <SidebarProvider>
-        <AppSidebar />
-        <main className='w-full'>
-          <div className='overflow-y-scroll h-full scroll-custom bg-[#282828] font-sans antialiased'>
+      <AppSidebar />
+      <main className='w-full'>
+        <div className='overflow-y-scroll h-full scroll-custom bg-[#282828] font-sans antialiased'>
+          <Suspense fallback={<RippleLoader />}>
             {children}
-            <Toaster />
-          </div>
-        </main>
+          </Suspense>
+          <Toaster />
+        </div>
+      </main>
     </SidebarProvider>
   )
 }
